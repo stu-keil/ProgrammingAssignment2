41240780c505f9ed07c57830feb5afb24938683f
STUKEIL <stephane.keil@gmail.com>
2014-11-23 13:25:01 -0600

    Added some comments to the R script for verbose

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,8 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file is part of the Programming Assignment 2 of the R programming course session 09 from John Hopkins University
+## The purpose of these functions are to show how lexical scoping can be used to avoid lengthy computations when calculating
+## the inverse of a Matrix. These functions create a list of four functions with setter and getter methods in the parent.environment
 
-## Write a short comment describing this function
+## Creates a list of four functions that allow to modify the contents of the input matrix and it´s inverse
 
 makeCacheMatrix <- function(x = matrix()) {
     minv <- NULL
@@ -20,7 +21,7 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 
-## Write a short comment describing this function
+## Receives the list of functions and access the original matrix to calculate the inverse if it has not yet been calculated.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
